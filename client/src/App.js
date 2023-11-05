01423462e138426f0d64d2f71d3adc2056d7676d
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-05 13:59:43 +0530

    feat: add client and basic setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container"></div>
+    </>
+  );
+}
+
+export default App;
